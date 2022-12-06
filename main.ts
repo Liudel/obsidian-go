a6809eb33395f7e4811df5ea977e92513f104bb4
wangruoyu <wangruoyu@shizhuang-inc.com>
2022-12-06 20:57:38 +0800

    基本完成

--- main.ts
+++ main.ts
@@ -1,137 +1,197 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { request } from "http";
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	MarkdownPostProcessorContext,
+	MarkdownRenderer,
+	View,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
+let id = 0;
 
-interface MyPluginSettings {
+function getId() {
+	return id++;
+}
+
+function createRespText(el: HTMLElement, id: string, text: string) {
+	let pre = el.createEl("pre", {
+		cls: "language-text",
+		attr: {
+			id: id,
+			tabindex: "0",
+		},
+	});
+
+	pre.append(
+		el.createEl("code", {
+			cls: "language-text is-loaded",
+			text: text,
+		})
+	);
+
+	let del = el.createEl("button", {
+		cls: "copy-code-button",
+		text: "删除",
+	});
+
+	del.addEventListener("click", function (e: Event) {
+		let output = el.querySelectorAll("#" + id);
+		if (output != null) {
+			output.forEach(function (
+				v: Element,
+				k: number,
+				parent: NodeListOf<Element>
+			) {
+				pre.parentElement?.removeChild(v);
+			});
+		}
+	});
+
+	pre.append(del);
+
+	el.append(pre);
+}
+
+interface GolangPluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: GolangPluginSettings = {
+	mySetting: "default",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class GolangPlugin extends Plugin {
+	settings: GolangPluginSettings;
 
 	async onload() {
+		console.log("Loading Run go");
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerMarkdownPostProcessor(this.postprocessor.bind(this));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async postprocessor(el: HTMLElement, ctx: MarkdownPostProcessorContext) {
+		let pres = Array.from(el.querySelectorAll("pre"));
+
+		for (const pre of pres) {
+			let section = ctx.getSectionInfo(pre);
+			if (section == null) {
+				continue;
+			}
+
+			let sectionArr = section?.text.split("\n");
+			if (sectionArr?.at(section.lineStart) != "```go") {
+				console.log(sectionArr?.at(section.lineStart));
+				continue;
+			}
+
+			// console.log(.join('\n'))
+
+			let button = el.createEl("button", {
+				text: "Run",
+				cls: "copy-code-button",
+			});
+
+			function addResp(pre: HTMLPreElement) {
+				let id = "ob-go" + getId();
+				console.log(id)
+				return function (e: Event) {
+					if (section == null) {
+						console.log("section is null");
+						return;
+					}
+
+					var myHeaders = new Headers();
+					myHeaders.append(
+						"content-type",
+						"application/x-www-form-urlencoded; charset=UTF-8"
+					);
+
+					var raw =
+						"version=2&body=" +
+						encodeURIComponent(
+							sectionArr
+								.slice(section.lineStart + 1, section.lineEnd)
+								.join("\n")
+						) +
+						"&withVet=true";
+					console.log(raw);
+
+					var requestOptions: RequestInit = {
+						method: "POST",
+						headers: myHeaders,
+						body: raw,
+						redirect: "follow",
+					};
+
+					fetch(
+						"http://www.liudel.top/compile?backend=",
+						requestOptions
+					)
+						.then((response) => response.text())
+						.then((result) => {
+							let res = JSON.parse(result);
+							console.log(res, res.Events[0].Message);
+							let text = res.Errors;
+							if (text == "") {
+								text = res.Events[0].Message;
+							}
+							let output = el.querySelectorAll("#" + id);
+							if (output != null) {
+								output.forEach(function (
+									v: Element,
+									k: number,
+									parent: NodeListOf<Element>
+								) {
+									pre.parentElement?.removeChild(v);
+								});
+							}
+							createRespText(
+								pre.parentElement,
+								id,
+								"** OUTPUT **\n\n" + text + "\n"
+							);
+						})
+						.catch((error) => console.log("error", error));
+				};
+			}
+
+			button.addEventListener("click", addResp(pre));
+			pre.append(button);
+		}
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class GolangSettingTab extends PluginSettingTab {
+	plugin: GolangPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: GolangPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
